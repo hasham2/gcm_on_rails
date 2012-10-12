9c9eb9eb63e84111f6206ddf792dc8bc611ee12a
Hasham Malik <hasham2@gmail.com>
2012-10-12 18:26:32 +0500

    Getting Gcm requests to work and fixing issues

--- lib/gcm_on_rails/app/models/gcm/device.rb
+++ lib/gcm_on_rails/app/models/gcm/device.rb
@@ -8,30 +8,32 @@
 #
 # Example:
 #   Device.create(:registration_id => 'FOOBAR')
-class Gcm::Device
-  include Mongoid::Document
-  include Mongoid::Timestamps
-  store_in collection: "gcm_devices" 
+module Gcm
+  class Gcm::Device
+    include Mongoid::Document
+    include Mongoid::Timestamps
+    store_in collection: "gcm_devices" 
   
-  field :registration_id, type: String
-  field :last_registered_at, type: DateTime
+    field :registration_id, type: String
+    field :last_registered_at, type: DateTime
   
-  attr_accessible :registration_id
+    attr_accessible :registration_id
 
-  has_many :notifications, class_name: 'Gcm::Notification', dependent: :destroy
+    has_many :notifications, class_name: 'Gcm::Notification', dependent: :destroy
   
-  validates_presence_of :registration_id
-  validates_uniqueness_of :registration_id
+    validates_presence_of :registration_id
+    validates_uniqueness_of :registration_id
 
-  before_save :set_last_registered_at
+    before_save :set_last_registered_at
 
-  # The <tt>feedback_at</tt> accessor is set when the
-  # device is marked as potentially disconnected from your
-  # application by Google.
-  attr_accessor :feedback_at
+    # The <tt>feedback_at</tt> accessor is set when the
+    # device is marked as potentially disconnected from your
+    # application by Google.
+    attr_accessor :feedback_at
 
-  private
-  def set_last_registered_at
-    self.last_registered_at = Time.now if self.last_registered_at.nil?
+    private
+    def set_last_registered_at
+      self.last_registered_at = Time.now if self.last_registered_at.nil?
+    end
   end
 end
--- lib/gcm_on_rails/app/models/gcm/notification.rb
+++ lib/gcm_on_rails/app/models/gcm/notification.rb
@@ -1,93 +1,96 @@
-class Gcm::Notification
-  include Mongoid::Document
-  include Mongoid::Timestamps
-  store_in collection: "gcm_notifications"
+module Gcm
+  class Gcm::Notification
+    include Mongoid::Document
+    include Mongoid::Timestamps
+    store_in collection: "gcm_notifications"
 
-  include ::ActionView::Helpers::TextHelper
-  extend ::ActionView::Helpers::TextHelper
+    include ::ActionView::Helpers::TextHelper
+    extend ::ActionView::Helpers::TextHelper
   
-  field :collapse_key, type: String
-  field :data, type: Hash
-  field :delay_while_idle, type: Boolean
-  field :sent_at, type: DateTime
-  field :time_to_live, type: Integer
+    field :collapse_key, type: String
+    field :data, type: Hash
+    field :delay_while_idle, type: Boolean
+    field :sent_at, type: DateTime
+    field :time_to_live, type: Integer
 
-  #serialize :data
+    #serialize :data
 
-  belongs_to :device, :class_name => 'Gcm::Device'
-  validates_presence_of :collapse_key, :if => :time_to_live?
+    belongs_to :device, :class_name => 'Gcm::Device'
+    validates_presence_of :collapse_key, :if => :time_to_live?
 
-  class << self
-    # Opens a connection to the Google GCM server and attempts to batch deliver
-    # an Array of notifications.
-    #
-    # This method expects an Array of Gcm::Notifications. If no parameter is passed
-    # in then it will use the following:
-    #   Gcm::Notification.all(:conditions => {:sent_at => nil})
-    #
-    # As each Gcm::Notification is sent the <tt>sent_at</tt> column will be timestamped,
-    # so as to not be sent again.
-    #
-    # This can be run from the following Rake task:
-    #   $ rake gcm:notifications:deliver
-    def send_notifications(notifications = Gcm::Notification.all(:conditions => {:sent_at => nil}, :joins => :device, :readonly => false))
+    class << self
+      # Opens a connection to the Google GCM server and attempts to batch deliver
+      # an Array of notifications.
+      #
+      # This method expects an Array of Gcm::Notifications. If no parameter is passed
+      # in then it will use the following:
+      #   Gcm::Notification.all(:conditions => {:sent_at => nil})
+      #
+      # As each Gcm::Notification is sent the <tt>sent_at</tt> column will be timestamped,
+      # so as to not be sent again.
+      #
+      # This can be run from the following Rake task:
+      #   $ rake gcm:notifications:deliver
+      def send_notifications(notifications = Gcm::Notification.where(sent_at: nil))
 
-      if configatron.gcm_on_rails.delivery_format and configatron.gcm_on_rails.delivery_format == 'plain_text'
-        format = "plain_text"
-      else
-        format = "json"
-      end
+        if configatron.gcm_on_rails.delivery_format and configatron.gcm_on_rails.delivery_format == 'plain_text'
+          format = "plain_text"
+        else
+          format = "json"
+        end
 
-      unless notifications.nil? || notifications.empty?
-        api_key = Gcm::Connection.open
-        if api_key
-          notifications.each do |notification|
-            #puts "sending notification #{notification.id} to device #{notification.device.registration_id}"
-            response = Gcm::Connection.send_notification(notification, api_key, format)
-            #puts "response: #{response[:code]}; #{response.inspect}"
-            if response[:code] == 200
-              if format == "json"
-                error = ""
-                message_data = JSON.parse response[:message]
-                success = message_data['success']
-                error = message_data['results'][0]['error']  if success == 0
-              else   #format is plain text
-                message_data = response[:message]
-                error = response[:message].split('=')[1]
+        unless notifications.nil? || notifications.empty?
+          api_key = Gcm::Connection.open
+          if api_key
+            notifications.each do |notification|
+              #puts "sending notification #{notification.id} to device #{notification.device.registration_id}"
+              response = Gcm::Connection.send_notification(notification, api_key, format)
+              #puts "response: #{response[:code]}; #{response.inspect}"
+              if response[:code] == 200
+                
+                if format == "json"
+                  error = ""
+                  message_data = JSON.parse response[:message]
+                  success = message_data['success']
+                  error = message_data['results'][0]['error']  if success == 0
+                else   #format is plain text
+                  message_data = response[:message]
+                  error = response[:message].split('=')[1]
+                end
+
+
+                case error
+                  when "MissingRegistration"
+                    ex = Gcm::Errors::MissingRegistration.new(response[:message])
+                    logger.warn("#{ex.message}, destroying gcm_device with id #{notification.device.id}")
+                    notification.device.destroy
+                  when "InvalidRegistration"
+                    ex = Gcm::Errors::InvalidRegistration.new(response[:message])
+                    logger.warn("#{ex.message}, destroying gcm_device with id #{notification.device.id}")
+                    notification.device.destroy
+                  when "MismatchedSenderId"
+                    ex = Gcm::Errors::MismatchSenderId.new(response[:message])
+                    logger.warn(ex.message)
+                  when "NotRegistered"
+                    ex = Gcm::Errors::NotRegistered.new(response[:message])
+                    logger.warn("#{ex.message}, destroying gcm_device with id #{notification.device.id}")
+                    notification.device.destroy
+                  when "MessageTooBig"
+                    ex = Gcm::Errors::MessageTooBig.new(response[:message])
+                    logger.warn(ex.message)
+                  else
+                   notification.sent_at = Time.now
+                   notification.save!
+                  end
+              elsif response[:code] == 401
+                raise Gcm::Errors::InvalidAuthToken.new(message_data)
+              elsif response[:code] == 503
+                raise Gcm::Errors::ServiceUnavailable.new(message_data)
+              elsif response[:code] == 500
+                raise Gcm::Errors::InternalServerError.new(message_data)
               end
 
-
-              case error
-                when "MissingRegistration"
-                  ex = Gcm::Errors::MissingRegistration.new(response[:message])
-                  logger.warn("#{ex.message}, destroying gcm_device with id #{notification.device.id}")
-                  notification.device.destroy
-                when "InvalidRegistration"
-                  ex = Gcm::Errors::InvalidRegistration.new(response[:message])
-                  logger.warn("#{ex.message}, destroying gcm_device with id #{notification.device.id}")
-                  notification.device.destroy
-                when "MismatchedSenderId"
-                  ex = Gcm::Errors::MismatchSenderId.new(response[:message])
-                  logger.warn(ex.message)
-                when "NotRegistered"
-                  ex = Gcm::Errors::NotRegistered.new(response[:message])
-                  logger.warn("#{ex.message}, destroying gcm_device with id #{notification.device.id}")
-                  notification.device.destroy
-                when "MessageTooBig"
-                  ex = Gcm::Errors::MessageTooBig.new(response[:message])
-                  logger.warn(ex.message)
-                else
-                  notification.sent_at = Time.now
-                  notification.save!
-              end
-            elsif response[:code] == 401
-              raise Gcm::Errors::InvalidAuthToken.new(message_data)
-            elsif response[:code] == 503
-              raise Gcm::Errors::ServiceUnavailable.new(message_data)
-            elsif response[:code] == 500
-              raise Gcm::Errors::InternalServerError.new(message_data)
             end
-
           end
         end
       end
